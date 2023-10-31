15fed6963e72865006e8468893d71bf99baf0dbf
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-10-31 11:56:28 +0100

    showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisability] = useState(false);
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisability(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisability(true)}>
         My Button
       </Button>
     </div>
